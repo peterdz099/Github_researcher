84af1e4b4da0691c69c12593631efa078688494a
PITERKE <piotrdziula@gmail.com>
2022-04-22 22:27:47 +0200

    added new activity

---
+++ app/src/main/java/com/example/github_researcher/Repositories.java
@@ -0,0 +1,14 @@
+package com.example.github_researcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Repositories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repositories);
+    }
+}
